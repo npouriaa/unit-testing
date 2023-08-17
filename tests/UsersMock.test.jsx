099004e15b7ec086aa46400ac3a20e68e4e8b92e
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-08-17 23:45:11 +0330

    adding beforeEach ,beforeAll, afterEach, aftereAll

--- tests/UsersMock.test.jsx
+++ tests/UsersMock.test.jsx
@@ -3,11 +3,27 @@ import UsersMock from "../src/components/UsersMock";
 import { render, screen } from "@testing-library/react";
 
 describe("UsersMock component", () => {
+  beforeEach(() => {
+    console.log("running the for each test");
+  });
+
+  beforeAll(() => {
+    console.log("running once before all tests");
+  });
+
+  afterEach(() => {
+    console.log("running the after each test");
+  });
+
+  afterAll(() => {
+    console.log("running once after all tests");
+  });
+
   test("should render users mocked item", async () => {
     render(<UsersMock />);
     const userItem = await screen.findByTestId("user-item-1");
     const userItemText = await screen.findByText("john");
-    screen.debug()
+    screen.debug();
     expect(userItem).toBeInTheDocument();
     expect(userItemText).toBeInTheDocument();
   });
